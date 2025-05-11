144798f5fb6253ea11e3452d3a6a410693021a11
Mohammed Hadi CA <122148880+mhadica@users.noreply.github.com>
2025-05-11 10:09:41 +0530

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -49,6 +49,7 @@ const Education = () => {
     "Python",
     "IoT",
     "Dbms"
+    "Big Data"
   ];
 
   return (
@@ -123,6 +124,18 @@ const Education = () => {
                   {course}
                 </div>
               ))}
+              <a
+            href="https://www.linkedin.com/in/mhadica/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="outline"
+              className="border-classic-navy text-classic-navy hover:bg-classic-navy/5 text-sm md:text-base"
+            >
+              View More Skills on LinkedIn
+            </Button>
+          </a>
             </div>
           </div>
         </div>
